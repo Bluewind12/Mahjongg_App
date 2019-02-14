83a6a1434e5e9895e36af80d13a7182887aaf5c5
Bluewind12 <zero18030@gmail.com>
2019-02-14 18:10:09 +0900

    表画面が出せなかった問題の解決

--- app/src/main/java/momonyan/mahjongg_tools/TableOutputActivity.kt
+++ app/src/main/java/momonyan/mahjongg_tools/TableOutputActivity.kt
@@ -3,28 +3,19 @@ package momonyan.mahjongg_tools
 import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.ImageButton
+import kotlinx.android.synthetic.main.score_table.*
 
 class TableOutputActivity : AppCompatActivity() {
-    private lateinit var imageView: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
-        val dataStore = getSharedPreferences("MainData", Context.MODE_PRIVATE)
-        when (dataStore.getString("Theme", "Dark")) {
-            "Dark" -> setTheme(R.style.DarkTheme)
-            "Light" -> setTheme(R.style.LightTheme)
-            "Mat" -> setTheme(R.style.MatTheme)
-            else -> error(2)
-        }
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.table_view)
-        imageView = findViewById(R.id.imageViewOutput)
+        setContentView(R.layout.score_table)
         when (intent.getStringExtra("image")) {
-            "oya_point" -> imageView.setImageResource(R.drawable.point_oya)
-            "ko_point" -> imageView.setImageResource(R.drawable.point_ko)
-            "fu" -> imageView.setImageResource(R.drawable.mark)
-            else -> imageView.setImageResource(R.drawable.hougaku4_kita)
+            "oya_point" -> tableImages.setImageResource(R.drawable.point_oya)
+            "ko_point" -> tableImages.setImageResource(R.drawable.point_ko)
+            "fu" -> tableImages.setImageResource(R.drawable.mark)
+            else -> tableImages.setImageResource(R.drawable.hougaku4_kita)
         }
-        imageView.setOnClickListener {
+        tableImages.setOnClickListener {
             finish()
         }
     }
