3defcb0188ec93b1f68995415db1885f18aee5b8
Bluewind12 <zero18030@gmail.com>
2019-04-16 03:10:47 +0900

    RecyclerViewでのマネージャの準備 ポジションによる移動を可能に

---
+++ app/src/main/java/momonyan/mahjongg_tools/RecyclerLayoutCustomManager.kt
@@ -0,0 +1,33 @@
+package momonyan.mahjongg_tools
+
+import android.content.Context
+import android.graphics.PointF
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.LinearSmoothScroller
+import android.support.v7.widget.RecyclerView
+
+
+class RecyclerLayoutCustomManager : LinearLayoutManager {
+
+    constructor(context: Context) : super(context, LinearLayoutManager.VERTICAL, false) {}
+
+    constructor(context: Context, orientation: Int, reverseLayout: Boolean) : super(context, orientation, reverseLayout) {}
+
+    override fun smoothScrollToPosition(recyclerView: RecyclerView, state: RecyclerView.State?,
+                                        position: Int) {
+        val smoothScroller = TopSnappedSmoothScroller(recyclerView.context)
+        smoothScroller.targetPosition = position
+        startSmoothScroll(smoothScroller)
+    }
+
+    private inner class TopSnappedSmoothScroller(context: Context) : LinearSmoothScroller(context) {
+
+        override fun computeScrollVectorForPosition(targetPosition: Int): PointF? {
+            return this.computeScrollVectorForPosition(targetPosition)
+        }
+
+        override fun getVerticalSnapPreference(): Int {
+            return SNAP_TO_START
+        }
+    }
+}
