832078cf87725e70c6035cee02c2ec24d8933d96
“BlueWind_Mac” <zero18030@gmail.com>
2019-10-02 22:45:28 +0900

    readme用のレイアウト準備

---
+++ app/src/main/java/momonyan/mahjongg_tools/activity/ReadMeActivity.kt
@@ -0,0 +1,12 @@
+package momonyan.mahjongg_tools.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import momonyan.mahjongg_tools.R
+
+class ReadMeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.readme_layout)
+    }
+}
