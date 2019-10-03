eb94a2388c092160c177a9edbdae47b0e657fb75
“BlueWind_Mac” <zero18030@gmail.com>
2019-10-03 16:32:55 +0900

    Readmeレイアウトのメニューの作成

--- app/src/main/java/momonyan/mahjongg_tools/activity/ReadMeActivity.kt
+++ app/src/main/java/momonyan/mahjongg_tools/activity/ReadMeActivity.kt
@@ -1,6 +1,9 @@
 package momonyan.mahjongg_tools.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import momonyan.mahjongg_tools.R
 
@@ -9,4 +12,27 @@ class ReadMeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.readme_layout)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.mark_option_menu, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.mark_back -> {
+                finish()
+                return true
+            }
+            R.id.mark_menu_home -> {
+                val intent = Intent(this, MainActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+            else -> Error("Menu Select Error")
+        }
+        return false
+    }
+
 }
